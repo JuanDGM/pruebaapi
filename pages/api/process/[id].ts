cb4b4979486d7683742a1a7e373d055497784a6d
Juan David Garcia <juan84_10@hotmail.com>
2022-07-12 09:41:04 -0500

    first commit

---
+++ pages/api/process/[id].ts
@@ -0,0 +1,12 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+type Data = {
+    name: any
+}
+
+export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+    
+    const {id} = req.query;
+    
+    res.status(200).json({ name: id })
+}
